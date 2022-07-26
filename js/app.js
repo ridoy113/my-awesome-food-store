58818e7496775956a20e627da51838270e7d255f
ridoy113 <almamounhridoy113@gmail.com>
2022-07-26 22:26:27 +0600

    make responsive complite

--- js/app.js
+++ js/app.js
@@ -5,5 +5,24 @@ $(document).ready(function () {
         prevArrow: ".prev-btn",
         nextArrow: ".next-btn",
         autoplay: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
     });
+
+    $('.nav-trigger').click(function () {
+        $('.site-content-wrapper').toggleClass('scaled');
+    })
 })
